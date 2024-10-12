320c488f40b5d16537dddd68d56681ee36b1be86
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2024-10-12 18:13:22 -0400

    More module reorg

--- dot_config/nvim/lua/kuba/init.lua
+++ dot_config/nvim/lua/kuba/init.lua
@@ -1,7 +1,6 @@
 require("kuba.set")
 require("kuba.remap")
 
--- Highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
@@ -21,7 +20,6 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
--- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
@@ -44,14 +42,10 @@ require("lazy").setup({
 		event = "VimEnter", -- Sets the loading event to 'VimEnter'
 		opts = {
 			icons = {
-				-- set icon mappings to true if you have a Nerd Font
 				mappings = vim.g.have_nerd_font,
-				-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-				-- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
 				keys = vim.g.have_nerd_font and {},
 			},
 
-			-- Document existing key chains
 			spec = {
 				{ "<leader>c", group = "[C]ode", mode = { "n", "x" } },
 				{ "<leader>d", group = "[D]ocument" },
@@ -72,25 +66,15 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
 				"nvim-telescope/telescope-fzf-native.nvim",
-
-				-- `build` is used to run some command when the plugin is installed/updated.
-				-- This is only run then, not every time Neovim starts up.
 				build = "make",
-
-				-- `cond` is a condition used to determine whether this plugin should be
-				-- installed and loaded.
 				cond = function()
 					return vim.fn.executable("make") == 1
 				end,
 			},
 			{ "nvim-telescope/telescope-ui-select.nvim" },
-
-			-- Useful for getting pretty icons, but requires a Nerd Font.
 			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 		},
 		config = function()
-			-- [[ Configure Telescope ]]
-			-- See `:help telescope` and `:help telescope.setup()`
 			require("telescope").setup({
 				-- You can put your default mappings / updates / etc. in here
 				--  All the info you're looking for is in `:help telescope.setup()`
@@ -148,8 +132,6 @@ require("lazy").setup({
 
 	-- LSP Plugins
 	{
-		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-		-- used for completion, annotations and signatures of Neovim apis
 		"folke/lazydev.nvim",
 		ft = "lua",
 		opts = {
@@ -291,7 +273,6 @@ require("lazy").setup({
 				jsonls = {},
 				pyright = {},
 				ruff = {},
-				yamlls = {},
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes = { ...},
@@ -474,9 +455,6 @@ require("lazy").setup({
 							luasnip.jump(-1)
 						end
 					end, { "i", "s" }),
-
-					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 				}),
 				sources = {
 					{
@@ -506,23 +484,6 @@ require("lazy").setup({
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
 	},
-	--{ -- You can easily change to a different colorscheme.
-	--	-- Change the name of the colorscheme plugin below, and then
-	--	-- change the command in the config to whatever the name of that colorscheme is.
-	--	--
-	--	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	--	"folke/tokyonight.nvim",
-	--	priority = 1000, -- Make sure to load this before all the other start plugins.
-	--	init = function()
-	--		-- Load the colorscheme here.
-	--		-- Like many other themes, this one has different styles, and you could load
-	--		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-	--		vim.cmd.colorscheme("tokyonight-night")
-
-	--		-- You can configure highlights by doing something like:
-	--		vim.cmd.hi("Comment gui=none")
-	--	end,
-	--},
 
 	-- Highlight todo, notes, etc in comments
 	{
@@ -564,9 +525,6 @@ require("lazy").setup({
 			statusline.section_location = function()
 				return "%2l:%-2v"
 			end
-
-			-- ... and there is more!
-			--  Check out: https://github.com/echasnovski/mini.nvim
 		end,
 	},
 	{ -- Highlight, edit, and navigate code
@@ -631,8 +589,6 @@ require("lazy").setup({
 	-- { import = 'custom.plugins' },
 }, {
 	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
 		icons = vim.g.have_nerd_font and {},
 	},
 })
