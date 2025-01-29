9b07a861123cad0cb0dac9bd966ca8af1c49b1e3
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-01-29 10:00:39 -0500

    Minor revisions

--- dot_config/nvim/lua/kuba/plugins/colors.lua
+++ dot_config/nvim/lua/kuba/plugins/colors.lua
@@ -12,12 +12,6 @@ return {{
 		ft = "python",
 		build = ":UpdateRemotePlugins",
 		init = function()
-			-- Disabled these features better provided by LSP or other more general plugins
-			--vim.g["semshi#error_sign"] = false
-			--vim.g["semshi#simplify_markup"] = false
-			--vim.g["semshi#mark_selected_nodes"] = false
-			--vim.g["semshi#update_delay_factor"] = 0.001
-
 			-- This autocmd must be defined in init to take effect
 			vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
 				group = vim.api.nvim_create_augroup("SemanticHighlight", {}),
