0846768955c8a0503a6f1cb0c7398f0f46e5ceb8
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-22 17:18:32 -0400

    Simplify remaps

--- dot_config/nvim/lua/kuba/remap.lua
+++ dot_config/nvim/lua/kuba/remap.lua
@@ -1,12 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>bs", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>bc", "<cmd>enew<CR>")
 vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>")
 vim.keymap.set("n", "<leader>bp", "<cmd>bprev<CR>")
 vim.keymap.set("n", "<leader>bk", "<cmd>bd<CR>")
@@ -18,15 +14,8 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
-vim.keymap.set("n", "J", "mzJ`z")
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("i", "kj", "<Esc>")
 vim.keymap.set("i", "<C-h>", "<Left>")
@@ -37,9 +26,11 @@ vim.keymap.set("i", "<C-k>", "<Up>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new-window tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Netrw remaps
 vim.api.nvim_create_autocmd("filetype", {
--- dot_config/nvim/lua/kuba/set.lua
+++ dot_config/nvim/lua/kuba/set.lua
@@ -1,27 +1,19 @@
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
-
-vim.opt.showmode = false
-
-vim.opt.undofile = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.opt.inccommand = "split"
-
 vim.opt.scrolloff = 10
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.softtabstop = 4
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 4
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
