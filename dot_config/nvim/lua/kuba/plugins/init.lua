10fbee8cdaa319fbf858cd0daa85914edd9c389b
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-23 11:15:26 -0400

    One file is simpler than multple

--- dot_config/nvim/lua/kuba/plugins/init.lua
+++ dot_config/nvim/lua/kuba/plugins/init.lua
@@ -1,28 +1,465 @@
 return {
-	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	------------------------ COLORS ------------------------
 	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		dependencies = { "hrsh7th/nvim-cmp" },
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			require("nvim-autopairs").setup({})
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp = require("cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			vim.cmd.colorscheme("moonfly")
 		end,
 	},
 	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
+		"wookayin/semshi",
+		ft = "python",
+		build = ":UpdateRemotePlugins",
+		init = function()
+			-- This autocmd must be defined in init to take effect
+			vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
+				group = vim.api.nvim_create_augroup("SemanticHighlight", {}),
+				callback = function()
+					-- Only add style, inherit or link to the LSP's colors
+					vim.cmd([[
+            highlight! semshiGlobal gui=italic
+            highlight! semshiImported gui=bold
+            highlight! link semshiParameter @lsp.type.parameter
+            highlight! link semshiParameterUnused DiagnosticUnnecessary
+            highlight! link semshiBuiltin @function.builtin
+            highlight! link semshiAttribute @attribute
+            highlight! link semshiSelf @lsp.type.selfKeyword
+            highlight! link semshiUnresolved @lsp.type.unresolvedReference
+            ]])
+				end,
+			})
+		end,
+	},
+
+	------------------------ FORMAT ------------------------
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_format = "fallback" })
+				end,
+				mode = "",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+			formatters_by_ft = {
+				sh = { "beautysh" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				lua = { "stylua" },
+				python = { "ruff" },
+			},
+		},
+	},
+
+	------------------------ FUGITIVE ------------------------
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+			local Kuba_Fugitive = vim.api.nvim_create_augroup("Kuba_Fugitive", {})
+			local autocmd = vim.api.nvim_create_autocmd
+			autocmd("BufWinEnter", {
+				group = Kuba_Fugitive,
+				pattern = "*",
+				callback = function()
+					if vim.bo.ft ~= "fugitive" then
+						return
+					end
+
+					local bufnr = vim.api.nvim_get_current_buf()
+					local opts = { buffer = bufnr, remap = false }
+					vim.keymap.set("n", "<leader>p", function()
+						vim.cmd.Git("push")
+					end, opts)
+
+					vim.keymap.set("n", "<leader>P", ":Git pull --rebase <CR>", opts)
+					vim.keymap.set("n", "<leader>l", ":Git lg<CR>", opts)
+					vim.keymap.set("n", "<leader>tr", ":Git tr<CR>", opts)
+					vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+				end,
+			})
+		end,
+	},
+	------------------------ HARPOON ------------------------
+	{
+		"theprimeagen/harpoon",
+		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
-	},
-	{ -- Collection of various small independent plugins/modules
-		"echasnovski/mini.nvim",
 		config = function()
-			-- Better Around/Inside textobjects
-			require("mini.ai").setup({ n_lines = 500 })
-			require("mini.surround").setup()
+			require("harpoon"):setup()
 		end,
+		keys = {
+			{
+				"<leader>a",
+				function()
+					require("harpoon"):list():add()
+				end,
+			},
+			{
+				"<C-e>",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+			},
+			{
+				"<leader>1",
+				function()
+					require("harpoon"):list():select(1)
+				end,
+			},
+			{
+				"<leader>2",
+				function()
+					require("harpoon"):list():select(2)
+				end,
+			},
+			{
+				"<leader>3",
+				function()
+					require("harpoon"):list():select(3)
+				end,
+			},
+			{
+				"<leader>4",
+				function()
+					require("harpoon"):list():select(4)
+				end,
+			},
+			{
+				"<leader>5",
+				function()
+					require("harpoon"):list():select(5)
+				end,
+			},
+		},
+	},
+
+	------------------------ LINTING ------------------------
+	{
+		{
+			"mfussenegger/nvim-lint",
+			event = { "BufReadPre", "BufNewFile" },
+			config = function()
+				local lint = require("lint")
+				lint.linters_by_ft = {
+					bash = { "bash" },
+					--c = { "clangtidy" },
+					--cpp = { "clangtidy" },
+					python = { "ruff" },
+					yaml = { "yamllint" },
+				}
+
+				local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+				vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+					group = lint_augroup,
+					callback = function()
+						lint.try_lint()
+					end,
+				})
+			end,
+		},
+	},
+
+	------------------------ LSP ------------------------
+	{
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+		{ "Bilal2453/luvit-meta", lazy = true },
+		{
+			"neovim/nvim-lspconfig",
+			dependencies = {
+				{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+				"williamboman/mason-lspconfig.nvim",
+				"WhoIsSethDaniel/mason-tool-installer.nvim",
+				{ "j-hui/fidget.nvim", opts = {} },
+				"hrsh7th/cmp-nvim-lsp",
+			},
+			config = function()
+				vim.api.nvim_create_autocmd("LspAttach", {
+					group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+					callback = function(event)
+						local map = function(keys, func, desc, mode)
+							mode = mode or "n"
+							vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+						end
+
+						-- Jump to the definition of the word under your cursor.
+						--  To jump back, press <C-t>.
+						map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+						--  Jump to Declaration.
+						map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+						-- Find references for the word under your cursor.
+						map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+						-- Jump to the implementation of the word under your cursor.
+						map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+						-- Fuzzy find all the symbols in your current document.
+						map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+						-- Rename the variable under your cursor.
+						map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+						-- The following two autocommands are used to highlight references of the
+						-- word under your cursor when your cursor rests there for a little while.
+						-- When you move your cursor, the highlights will be cleared (the second autocommand).
+						local client = vim.lsp.get_client_by_id(event.data.client_id)
+						if
+							client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
+						then
+							local highlight_augroup =
+								vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+							vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+								buffer = event.buf,
+								group = highlight_augroup,
+								callback = vim.lsp.buf.document_highlight,
+							})
+
+							vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+								buffer = event.buf,
+								group = highlight_augroup,
+								callback = vim.lsp.buf.clear_references,
+							})
+
+							vim.api.nvim_create_autocmd("LspDetach", {
+								group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+								callback = function(event2)
+									vim.lsp.buf.clear_references()
+									vim.api.nvim_clear_autocmds({
+										group = "kickstart-lsp-highlight",
+										buffer = event2.buf,
+									})
+								end,
+							})
+						end
+
+						-- Creates a keymap to toggle inlay hints, if the language server supports them
+						if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+							map("<leader>th", function()
+								vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+							end, "[T]oggle Inlay [H]ints")
+						end
+					end,
+				})
+
+				local capabilities = vim.lsp.protocol.make_client_capabilities()
+				capabilities =
+					vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+				local servers = {
+					bashls = {},
+					clangd = { cmd = { "clangd", "--background-index", "--clang-tidy" } },
+					ruff = {},
+					lua_ls = {
+						settings = {
+							Lua = {
+								completion = {
+									callSnippet = "Replace",
+								},
+							},
+						},
+					},
+				}
+
+				require("mason").setup()
+
+				local ensure_installed = vim.tbl_keys(servers or {})
+				vim.list_extend(ensure_installed, {
+					"stylua",
+				})
+				require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+				require("mason-lspconfig").setup({
+					handlers = {
+						function(server_name)
+							local server = servers[server_name] or {}
+							server.capabilities =
+								vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+							require("lspconfig")[server_name].setup(server)
+						end,
+					},
+				})
+			end,
+		},
+		{ -- Autocompletion
+			"hrsh7th/nvim-cmp",
+			event = "InsertEnter",
+			dependencies = {
+				{
+					"L3MON4D3/LuaSnip",
+					build = (function()
+						-- Build Step is needed for regex support in snippets.
+						if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+							return
+						end
+						return "make install_jsregexp"
+					end)(),
+					dependencies = {},
+				},
+				"saadparwaiz1/cmp_luasnip",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-path",
+			},
+			config = function()
+				local cmp = require("cmp")
+				local luasnip = require("luasnip")
+				luasnip.config.setup({})
+
+				cmp.setup({
+					snippet = {
+						expand = function(args)
+							luasnip.lsp_expand(args.body)
+						end,
+					},
+					completion = { completeopt = "menu,menuone,noinsert" },
+					mapping = cmp.mapping.preset.insert({
+						["<C-n>"] = cmp.mapping.select_next_item(),
+						["<C-p>"] = cmp.mapping.select_prev_item(),
+						["<C-b>"] = cmp.mapping.scroll_docs(-4),
+						["<C-f>"] = cmp.mapping.scroll_docs(4),
+						["<C-y>"] = cmp.mapping.confirm({ select = true }),
+						["<C-Space>"] = cmp.mapping.complete({}),
+					}),
+					sources = {
+						{
+							name = "lazydev",
+							group_index = 0,
+						},
+						{ name = "nvim_lsp" },
+						{ name = "luasnip" },
+						{ name = "path" },
+					},
+				})
+			end,
+		},
+	},
+
+	------------------------ TELESCOPE ------------------------
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VimEnter",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+
+			-- Enable Telescope extensions if they are installed
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			vim.keymap.set("n", "<leader>/", function()
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "[/] Fuzzily search in current buffer" })
+
+			vim.keymap.set("n", "<leader>s/", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[S]earch [/] in Open Files" })
+		end,
+	},
+
+	------------------------ TREESITTER ------------------------
+	{
+		{
+			"nvim-treesitter/nvim-treesitter",
+			build = ":TSUpdate",
+			main = "nvim-treesitter.configs",
+			opts = {
+				ensure_installed = {
+					"bash",
+					"c",
+					"cpp",
+					"lua",
+					"markdown",
+					"python",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			},
+		},
+		{
+			"fei6409/log-highlight.nvim",
+			config = function()
+				require("log-highlight").setup({})
+			end,
+		},
+	},
+
+	------------------------ MISC ------------------------
+	{
+		"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+		{
+			"windwp/nvim-autopairs",
+			event = "InsertEnter",
+			dependencies = { "hrsh7th/nvim-cmp" },
+			config = function()
+				require("nvim-autopairs").setup({})
+				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+				local cmp = require("cmp")
+				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			end,
+		},
+		{
+			"folke/todo-comments.nvim",
+			event = "VimEnter",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			opts = { signs = false },
+		},
+		{ -- Collection of various small independent plugins/modules
+			"echasnovski/mini.nvim",
+			config = function()
+				-- Better Around/Inside textobjects
+				require("mini.ai").setup({ n_lines = 500 })
+				require("mini.surround").setup()
+			end,
+		},
 	},
 }
