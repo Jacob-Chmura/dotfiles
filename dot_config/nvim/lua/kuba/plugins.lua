fc27d990b61bd4cd60368a2a7e28cba6857e92b5
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-12-03 16:01:41 -0500

    clean up

--- dot_config/nvim/lua/kuba/plugins.lua
+++ dot_config/nvim/lua/kuba/plugins.lua
@@ -1,12 +1,10 @@
 return {
 	------------------------ MASON ------------------------
-	-- Mason: Core package manager for LSP, DAP, linters, and formatters
 	{
 		"williamboman/mason.nvim",
 		cmd = "Mason", -- Define a command to open Mason's UI
 		opts = {}, -- Empty table for default options, or add custom configurations
 	},
-	-- Mason-LSPConfig: Bridge between Mason and nvim-lspconfig
 	{
 		"williamboman/mason-lspconfig.nvim",
 		-- Ensure this loads after mason.nvim and before nvim-lspconfig
@@ -270,32 +268,18 @@ return {
 
 			-- use a release tag to download pre-built binaries
 			version = "1.*",
-			-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-			-- build = 'cargo build --release',
-			-- If you use nix, you can build from source using latest nightly rust with:
-			-- build = 'nix run .#build-plugin',
 
 			---@module 'blink.cmp'
 			---@type blink.cmp.Config
 			opts = {
-				-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-				-- 'super-tab' for mappings similar to vscode (tab to accept)
-				-- 'enter' for enter to accept
-				-- 'none' for no mappings
-				--
-				--
 				-- All presets have the following mappings:
 				-- C-space: Open menu or open docs if already open
 				-- C-n/C-p or Up/Down: Select next/previous item
 				-- C-e: Hide menu
 				-- C-k: Toggle signature help (if signature.enabled = true)
-				--
-				-- See :h blink-cmp-config-keymap for defining your own keymap
 				keymap = { preset = "default" },
-
 				appearance = {
 					-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-					-- Adjusts spacing to ensure icons are aligned
 					nerd_font_variant = "mono",
 				},
 
@@ -311,13 +295,10 @@ return {
 				-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
 				-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
 				-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-				--
-				-- See the fuzzy documentation for more information
 				fuzzy = { implementation = "prefer_rust_with_warning" },
 			},
 			opts_extend = { "sources.default" },
 		},
-
 		{
 			"ray-x/lsp_signature.nvim",
 			event = "VeryLazy",
@@ -333,9 +314,8 @@ return {
 			end,
 		},
 
-		-- toml
+		-- toml / yaml
 		"cespare/vim-toml",
-		-- yaml
 		{
 			"cuducos/yaml.nvim",
 			ft = { "yaml" },
@@ -481,7 +461,6 @@ return {
 				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 			end,
 		},
-		-- auto-cd to root of git project
 		{
 			"notjedi/nvim-rooter.lua",
 			config = function()
